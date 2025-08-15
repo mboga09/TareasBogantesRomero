22fb931539dd4ebf0e094893709ebf8ffbc41f78
María José Bogantes Murillo <mariab9@estudiantec.cr>
2025-08-14 19:30:37 -0600

    Update archivo_con_errores.py

--- Tarea 1/archivo_con_errores.py
+++ Tarea 1/archivo_con_errores.py
@@ -1,7 +1,7 @@
 def bad():
-    l = 1  # E741: ambiguous variable name 'l'
-    if True: print("hi")  # E701: multiple statements on one line (colon)
+    l = 1  # E741: nombre de variable ambiguo 'l'
+    if True: print("hi")  # E701: múltiples sentencias en una sola línea (dos puntos)
     return l
 
 def also_bad():
-    return 42
+    return 42 # E302: se esperaban 2 líneas en blanco antes de la definición de la función
