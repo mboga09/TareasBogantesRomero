73441923930c5be1135a246e33791538811cf2da
María José Bogantes Murillo <mariab9@estudiantec.cr>
2025-08-14 19:33:41 -0600

    Update archivo_corregido.py

--- Tarea 1/archivo_corregido.py
+++ Tarea 1/archivo_corregido.py
@@ -1,9 +1,9 @@
 def bad():
-    value = 1
-    if True:
-        print("hi")
+    value = 1 # Nombre de variable descriptivo, no hay E741
+    if True: # Evitar usar constantes siempre True, mejor eliminar o documentar
+        print("hi") # No hay múltiples sentencias en una línea
     return value
 
 
 def also_bad():
-    return 42
+    return 42 # E302: se esperaban 2 líneas en blanco antes de la definición de la función (PEP8)
